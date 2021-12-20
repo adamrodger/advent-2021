376958c94a854660fe85052f509f2ea61b0e2090
Adam Rodger <adam.rodger@gmail.com>
2021-12-20 10:47:23 +0000

    Day 5 :snowflake:

---
+++ src/AdventOfCode/Day5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 5
+    /// </summary>
+    public class Day5
+    {
+        public int Part1(string[] input)
+        {
+            return Run(input, Part.One);
+        }
+
+        public int Part2(string[] input)
+        {
+            return Run(input, Part.Two);
+        }
+
+        private static int Run(string[] input, Part part)
+        {
+            Dictionary<Point2D, int> points = new Dictionary<Point2D, int>();
+
+            foreach (string line in input.Where(l => !string.IsNullOrWhiteSpace(l)))
+            {
+                int[] numbers = line.Numbers<int>();
+
+                (int x0, int y0, int x1, int y1) = (numbers[0], numbers[1], numbers[2], numbers[3]);
+
+                if (x0 == x1)
+                {
+                    // vertical
+                    for (int i = Math.Min(y0, y1); i <= Math.Max(y0, y1); i++)
+                    {
+                        Point2D point = (x0, i);
+                        points[point] = points.GetOrCreate(point) + 1;
+                    }
+                }
+                else if (y0 == y1)
+                {
+                    // horizontal
+                    for (int i = Math.Min(x0, x1); i <= Math.Max(x0, x1); i++)
+                    {
+                        Point2D point = (i, y0);
+                        points[point] = points.GetOrCreate(point) + 1;
+                    }
+                }
+                else if (part == Part.Two) // part 1 ignores diagonals
+                {
+                    // diagonal
+                    (int dx, int dy) = (x0 < x1, y0 < y1) switch
+                    {
+                        (true, true)   => ( 1,  1), // right and down
+                        (true, false)  => ( 1, -1), // right and up
+                        (false, true)  => (-1,  1), // left and down
+                        (false, false) => (-1, -1)  // left and up
+                    };
+
+                    Point2D current = (x0, y0);
+                    Point2D target = (x1 + dx, y1 + dy);
+
+                    while (current != target)
+                    {
+                        points[current] = points.GetOrCreate(current) + 1;
+                        current += (dx, dy);
+                    }
+                }
+            }
+
+            return points.Count(kvp => kvp.Value > 1);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day5Tests.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day5Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day5 solver;
+
+        public Day5Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day5();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day5.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2",
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 5;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 3990;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 5 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 12;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 21305;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 5 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
