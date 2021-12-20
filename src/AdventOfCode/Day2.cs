662a03db88b52fae71a236b71ee2239899c2d52a
Adam Rodger <adam.rodger@gmail.com>
2021-12-20 10:47:23 +0000

    Day 2 :snowman:

---
+++ src/AdventOfCode/Day2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 2
+    /// </summary>
+    public class Day2
+    {
+        public int Part1(string[] input)
+        {
+            int position = 0;
+            int depth = 0;
+
+            foreach (string line in input)
+            {
+                var parts = line.Split(' ');
+                int value = int.Parse(parts[1]);
+
+                (position, depth) = parts[0] switch
+                {
+                    "forward" => (position + value, depth),
+                    "down"    => (position,         depth + value),
+                    "up"      => (position,         depth - value),
+                    _ => throw new ArgumentOutOfRangeException(nameof(parts), parts[0], "Unsupported movement")
+                };
+            }
+
+            return position * depth;
+        }
+
+        public int Part2(string[] input)
+        {
+            int position = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string line in input)
+            {
+                var parts = line.Split(' ');
+                int value = int.Parse(parts[1]);
+
+                (position, depth, aim) = parts[0] switch
+                {
+                    "forward" => (position + value, depth + (aim * value), aim),
+                    "down"    => (position,         depth,                 aim + value),
+                    "up"      => (position,         depth,                 aim - value),
+                    _ => throw new ArgumentOutOfRangeException(nameof(parts), parts[0], "Unsupported movement")
+                };
+            }
+
+            return position * depth;
+        }
+    }
+}
