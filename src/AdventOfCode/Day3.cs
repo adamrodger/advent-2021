61d78acc982f311680f7835c5c695274d41867f4
Adam Rodger <adam.rodger@gmail.com>
2021-12-20 10:47:23 +0000

    Day 3 :deer:

---
+++ src/AdventOfCode/Day3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 3
+    /// </summary>
+    public class Day3
+    {
+        public int Part1(string[] input)
+        {
+            int length = input[0].Length;
+            int gamma = 0;
+            int epsilon = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                int shift = length - i - 1;
+                var ones = input.Count(line => line[i] == '1');
+
+                if (ones > input.Length / 2)
+                {
+                    gamma += 1 << shift;
+                }
+                else
+                {
+                    epsilon += 1 << shift;
+                }
+            }
+
+            int result = gamma * epsilon;
+            return result;
+        }
+
+        public int Part2(string[] input)
+        {
+            var oxygenCandidates = input.ToList();
+            var co2Candidates = input.ToList();
+
+            // oxygen keeps the candidates matching the most common bit in the current position (or 1 on tie break)
+            for (int i = 0; i < input[0].Length && oxygenCandidates.Count > 1; i++)
+            {
+                var ones = oxygenCandidates.Count(line => line[i] == '1');
+
+                if (ones >= (int)Math.Ceiling((double)oxygenCandidates.Count / 2))
+                {
+                    oxygenCandidates.RemoveAll(o => o[i] == '0');
+                }
+                else
+                {
+                    oxygenCandidates.RemoveAll(o => o[i] == '1');
+                }
+            }
+
+            // co2 keeps the candidates matching the least common bit in the current position (or 0 on tie break)
+            for (int i = 0; i < input[0].Length && co2Candidates.Count > 1; i++)
+            {
+                var ones = co2Candidates.Count(line => line[i] == '1');
+
+                if (ones >= (int)Math.Ceiling((double)co2Candidates.Count / 2))
+                {
+                    co2Candidates.RemoveAll(o => o[i] == '1');
+                }
+                else
+                {
+                    co2Candidates.RemoveAll(o => o[i] == '0');
+                }
+            }
+
+            int result = Convert.ToInt32(oxygenCandidates.First(), 2) * Convert.ToInt32(co2Candidates.First(), 2);
+            return result;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day3Tests.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day3Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day3 solver;
+
+        public Day3Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day3();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day3.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 749376;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 3 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 2372923;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 3 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_Sample()
+        {
+            string[] input =
+            {
+                "00100",
+                "11110",
+                "10110",
+                "10111",
+                "10101",
+                "01111",
+                "00111",
+                "11100",
+                "10000",
+                "11001",
+                "00010",
+                "01010"
+            };
+
+            var result = solver.Part2(input);
+
+            Assert.Equal(230, result);
+        }
+
+    }
+}
