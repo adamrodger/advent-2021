5151afc87567298eb8f6290e63f6560e60e08f34
Adam Rodger <adam.rodger@gmail.com>
2021-12-20 10:47:24 +0000

    Day 16, working for first 2 samples

---
+++ src/AdventOfCode/Day16.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 16
+    /// </summary>
+    public class Day16
+    {
+        public uint Part1(string[] input)
+        {
+            string line = input.First();
+
+            string binary = string.Join(string.Empty, line.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            TryParseOperator(binary, out OperatorPacket packet, out _);
+
+            return AddVersions(packet);
+        }
+
+        public uint Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+
+        private static bool TryParseLiteral(string binary, out LiteralPacket packet, out int taken)
+        {
+            uint version = Convert.ToUInt32(binary[..3], 2);
+            uint type = Convert.ToUInt32(binary[3..6], 2);
+
+            if (type != 4)
+            {
+                packet = null;
+                taken = 0;
+                return false;
+            }
+
+            int i = 6;
+            string value = "";
+            string chunk = "";
+
+            while (true)
+            {
+                chunk = binary[i..(i + 5)];
+                value += chunk[1..];
+                i += 5;
+
+                if (chunk.StartsWith("0"))
+                {
+                    break;
+                }
+            }
+
+            uint parsed = Convert.ToUInt32(value, 2);
+
+            packet = new LiteralPacket { Version = version, Type = type, Value = parsed };
+            taken = i;
+            return true;
+        }
+
+        private static bool TryParseOperator(string binary, out OperatorPacket packet, out int taken)
+        {
+            uint version = Convert.ToUInt32(binary[..3], 2);
+            uint type = Convert.ToUInt32(binary[3..6], 2);
+            char lengthType = binary[6];
+
+            int offset = 7;
+            offset += lengthType == '1' ? 11 : 15;
+
+            int length = (int)Convert.ToUInt32(binary[7..offset], 2);
+
+            packet = new OperatorPacket
+            {
+                Type = type,
+                Version = version,
+                Inner = new List<Packet>()
+            };
+
+            int parsedPackets = 0;
+            int end = offset + length;
+
+            while (true)
+            {
+                if (TryParseLiteral(binary[offset..], out LiteralPacket literal, out taken))
+                {
+                    packet.Inner.Add(literal);
+                    offset += taken;
+                }
+                // recurse into sub-operator if literal wasn't found
+                else if (TryParseOperator(binary[offset..], out OperatorPacket subOperator, out taken))
+                {
+                    packet.Inner.Add(subOperator);
+                    offset += taken;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unable to find either literal or operator packet");
+                }
+
+                parsedPackets++;
+
+                if (lengthType == '0' && offset >= end)
+                {
+                    break;
+                }
+
+                if (lengthType == '1' && parsedPackets == length)
+                {
+                    break;
+                }
+            }
+
+            taken = offset;
+            return true;
+        }
+
+        private static uint AddVersions(Packet packet)
+        {
+            uint result = packet.Version;
+
+            if (packet is OperatorPacket o)
+            {
+                foreach (Packet inner in o.Inner)
+                {
+                    result += AddVersions(inner);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public abstract class Packet
+    {
+        public uint Version { get; set; }
+
+        public uint Type { get; set; }
+    }
+
+    public class LiteralPacket : Packet
+    {
+        public uint Value { get; set; }
+    }
+
+    public class OperatorPacket : Packet
+    {
+        public ICollection<Packet> Inner { get; set; } = new List<Packet>();
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day16Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day16Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day16 solver;
+
+        public Day16Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day16();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day16.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_SampleInput1_ProducesCorrectResponse()
+        {
+            uint expected = 0;
+
+            var result = solver.Part1(new [] { "38006F45291200" });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_SampleInput2_ProducesCorrectResponse()
+        {
+            uint expected = 14;
+
+            var result = solver.Part1(new[] { "EE00D40C823060" });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            uint expected = 0;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 16 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        /*[Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            uint expected = 0;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }*/
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            uint expected = 0;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 16 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
