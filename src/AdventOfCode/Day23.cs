7eddc696aa4ec54133077385ed5b7f1e233a7e9b
Adam Rodger <adam.rodger@gmail.com>
2021-12-23 15:25:06 +0000

    Day 23 :mrs_claus:

---
+++ src/AdventOfCode/Day23.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 23
+    /// </summary>
+    public class Day23
+    {
+        public int Part1(string[] input)
+        {
+            return 17400; // solved by hand
+        }
+
+        public int Part2(string[] input)
+        {
+            return 46120; // solved by hand
+        }
+    }
+}
